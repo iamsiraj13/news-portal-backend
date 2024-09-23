149869b1f4ac39d2c9064c3d85fd49d159e88805
iamsiraj13 <iamsiraj13@gmail.com>
2024-09-23 11:54:02 +0600

    update new

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,6 +1,7 @@
 const { formidable } = require("formidable");
 const cloudinary = require("cloudinary").v2;
 const moment = require("moment");
+const util = require("util");
 
 const newsModel = require("../models/news.model");
 const imageModel = require("../models/gallery.model");
@@ -33,7 +34,7 @@ const addNews = async (req, res) => {
       slug: title[0].trim().split(" ").join("-"),
       category,
       description: description[0],
-      date: moment(new Date()).format("MMMM d, YYYY"), // Date format
+      date: moment().format("LL"), // Date format
       writerName: name,
       image: url,
     });
@@ -144,4 +145,99 @@ const getDashboardNews = async (req, res) => {
   }
 };
 
-module.exports = { addNews, getImages, addImages, getDashboardNews };
+const getDashboardNewsById = async (req, res) => {
+  try {
+    const { newsId } = req.params;
+    const news = await newsModel.findById(newsId);
+    return res.status(200).json({
+      news,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "Failed",
+      message: "Unable to get news, Please try again later.",
+    });
+  }
+};
+const updateNews = async (req, res) => {
+  const { newsId } = req.params;
+
+  // Cloudinary configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    secure: true,
+  });
+
+  try {
+    // Create a new formidable form instance
+    const form = formidable({});
+
+    // Parse the form with a callback
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        return res.status(400).json({
+          status: "Failed",
+          message: "Error parsing form data.",
+        });
+      }
+      // console.log(fields);
+      // console.log(files);
+
+      let url = fields.old_image[0];
+
+      if (Object.keys(files).length > 0) {
+        console.log("url" + url);
+
+        // Handle old image deletion from Cloudinary
+        const splitImage = url.split("/");
+        console.log("splitImage" + splitImage);
+        const imageName = splitImage[splitImage.length - 1];
+        const publicId = imageName.split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+
+        // Upload new image to Cloudinary
+        const data = await cloudinary.uploader.upload(
+          files.new_image[0].filepath,
+          {
+            folder: "news",
+          }
+        );
+        url = data.url;
+      }
+
+      // Update news with new fields and image URL
+      const news = await newsModel.findByIdAndUpdate(
+        newsId,
+        {
+          title: fields.title[0],
+          slug: fields.title[0].split(" ").join("-"),
+          description: fields.description[0],
+          image: url,
+        },
+        { new: true }
+      );
+
+      return res.status(200).json({
+        news,
+        message: "News updated successfully",
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "Failed",
+      message: "Unable to update news, Please try again later.",
+    });
+  }
+};
+module.exports = {
+  addNews,
+  getImages,
+  addImages,
+  getDashboardNews,
+  getDashboardNewsById,
+  updateNews,
+};
--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -3,18 +3,17 @@ const {
   getImages,
   addImages,
   getDashboardNews,
+  getDashboardNewsById,
+  updateNews,
 } = require("../controllers/news.controller");
 const { authGuard } = require("../middleware/middleware");
 
 const newsRoute = require("express").Router();
 
 newsRoute.post("/news/add", authGuard, addNews);
-
+newsRoute.put("/news/update/:newsId", authGuard, updateNews);
 newsRoute.get("/images", authGuard, getImages);
-
 newsRoute.post("/images/add", authGuard, addImages);
-
-// dashboard news
-
 newsRoute.get("/news", authGuard, getDashboardNews);
+newsRoute.get("/news/:newsId", authGuard, getDashboardNewsById);
 module.exports = newsRoute;
